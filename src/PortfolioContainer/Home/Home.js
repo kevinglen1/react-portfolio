2970d28cc5d1d8417588792f1327b9f57c77f782
Kevin <kevinpeterglen@gmail.com>
2022-04-27 20:39:21 -0400

    started project component

--- src/PortfolioContainer/Home/Home.js
+++ src/PortfolioContainer/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Profile from './Profile/Profile'
+import Project from './Project/Project'
 import Footer from './Footer/Footer'
 import "./Home.css"
 
@@ -7,6 +8,7 @@ export default function Home() {
     return (
         <div className='home-container'>
             <Profile/>
+            <Project/>
             <Footer/>
         </div>
     )
